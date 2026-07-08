59af7ace2122f0152823a6d1dd904f4b568262c7
Paiz <Francopaiz100@gmail.com>
2026-07-08 14:42:59 -0300

    Arreglo de token

--- services/auth.service.js
+++ services/auth.service.js
@@ -5,11 +5,8 @@ const SECRET_KEY = process.env.SECRET_KEY;
 
 export const login = (email, password) => {
 
-    if (email !== "admin@correo.com" || password !== "1234") {
-        return null;
-    }
-
-    const token = jwt.sign(
+    if (email === "admin@gmail.com" && password === "123456") {
+       const token = jwt.sign(
         { email },
         SECRET_KEY,
         { expiresIn: "1h" }
@@ -19,4 +16,10 @@ export const login = (email, password) => {
         message: "Login exitoso",
         token: `${token}`
     };
+    }
+    else {
+ return null;
+    }
+
+    
 };
